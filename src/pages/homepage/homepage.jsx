f302fcc80225403e244d42c61cea53b30c080070
Lavneesh Jaggi <lavneesh.jaggi25@protonmail.com>
2022-05-01 13:22:59 +0530

    update code

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './homepage.styles.scss';
+import "./homepage.styles.scss";
 
 const Homepage = () => (
-    <div className="homepage">
-        <Link className="sections selectSections" to="/gestures">
-            <h1 className="name">Gesture Translation</h1>
-        </Link>
-        <Link className="sections selectSections" to="/signlanguage">
-            <h1 className="name">Sign Language Translation</h1>
-        </Link>
-    </div>
+  <div className="homepage">
+    <Link className="sections selectSections" to="/gestures">
+      <h1 className="name">Gesture Translation</h1>
+    </Link>
+    <Link className="sections selectSections" to="/signlanguage">
+      <h1 className="name">Sign Language Translation</h1>
+    </Link>
+    <Link className="sections selectSections hide" to="/signlanguage2">
+      <h1 className="name">Sign Language Translation</h1>
+    </Link>
+  </div>
 );
 
 export default Homepage;
