c45287032003e04dbaf35f9dfdf2be48190e1252
Lavneesh Jaggi <lavneesh.jaggi25@protonmail.com>
2022-05-04 10:46:03 +0530

    update code

--- src/pages/sign-language/utilities.js
+++ src/pages/sign-language/utilities.js
@@ -17,31 +17,31 @@ export const drawRect = (
   imgHeight,
   ctx
 ) => {
-  for (let i = 0; i <= boxes.length; i++) {
-    if (boxes[i] && classes[i] && scores[i] > threshold) {
-      const [y, x, height, width] = boxes[i];
-      const text = classes[i];
+  // for (let i = 0; i <= boxes.length; i++) {
+  if (boxes[0] && classes[0] && scores[0] > threshold) {
+    const [y, x, height, width] = boxes[0];
+    const text = classes[0];
 
-      ans = labelMap[text]["name"];
+    ans = labelMap[text]["name"];
 
-      ctx.strokeStyle = labelMap[text]["color"];
-      ctx.lineWidth = 10;
-      ctx.fillStyle = "white";
-      ctx.font = "30px Arial";
+    ctx.strokeStyle = labelMap[text]["color"];
+    ctx.lineWidth = 10;
+    ctx.fillStyle = "white";
+    ctx.font = "30px Arial";
 
-      ctx.beginPath();
-      ctx.fillText(
-        labelMap[text]["name"] + " - " + Math.round(scores[i] * 100) / 100,
-        x * imgWidth,
-        y * imgHeight - 10
-      );
-      ctx.rect(
-        x * imgWidth,
-        y * imgHeight,
-        (width * imgWidth) / 2,
-        (height * imgHeight) / 1.5
-      );
-      ctx.stroke();
-    }
+    ctx.beginPath();
+    ctx.fillText(
+      labelMap[text]["name"] + " - " + Math.round(scores[0] * 100) / 100,
+      x * imgWidth,
+      y * imgHeight - 10
+    );
+    ctx.rect(
+      x * imgWidth,
+      y * imgHeight,
+      (width * imgWidth) / 2,
+      (height * imgHeight) / 1.5
+    );
+    ctx.stroke();
+    // }
   }
 };
